bd1669cd20e7c84d60e4782b33b21d20144f2cc9
Darren Steven <darren.steven@itbss.ac.id>
2023-04-26 21:10:02 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,10 @@
 <body>
     <h1>Hello world</h1>
     <p>Lorem ipsum dolor sit amet consectetur.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+        <li>tiga</li>
+    </ul>
 </body>
 </html>
